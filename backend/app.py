c28e94a27f83be0a279e208def507ea32d6f9baa
William Fontanez <william.r.fontanez@gmail.com>
2022-03-02 17:47:08 -0600

    Created frontend browser router and components folder
    Components are addItem.js, home.js,items.js

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-#from flask_cors import CORS
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'ap
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-#CORS(app)
+CORS(app)
 
 
 class Item(db.Model):
